ad270951bb4dab166ab3c75f8d672d36b3ba0213
ItsmeHJB <harrison.be@hotmail.co.uk>
2022-08-04 17:05:22 +0100

    Closer to goal of finding pages - tweak terms or something else is wrong

--- slur-detection-base.py
+++ slur-detection-base.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 from warcio.archiveiterator import ArchiveIterator
 import re
+import random
 
 
 def get_file(url):
@@ -14,10 +15,20 @@ def get_record_with_header(warc_file, header, value):
             return record
 
 
+def get_WET_text(record):
+    return record.content_stream().read().decode('utf-8').lower()
+
+
 prefix_url = 'https://data.commoncrawl.org/'
 warc_match = re.compile('warc')
 wet_sub = 'wet'
 ext_sub = 'warc.wet'
+
+slurs_file = "Data/slur_subset.csv"
+f_len = sum(1 for line in open(slurs_file)) - 1
+sample_size = round(f_len/5)
+skip = sorted(random.sample(range(1, f_len+1), f_len-sample_size))
+slurs = pd.read_csv(slurs_file, skiprows=skip)['word'].tolist()
 
 reader = pd.read_csv('Data/b75c614d-bb12-4083-9664-3ea424f6d4ce.csv', usecols=[0], chunksize=1000)
 for df in reader:
@@ -28,6 +39,7 @@ for df in reader:
         wet_url = prefix_url + re.sub(warc_match, ext_sub, re.sub(warc_match, wet_sub, row[0], count=1))
         wet_file = get_file(wet_url)
 
+        # Get WARC and associated WET record
         # warc_record = get_record_with_header(
         #     warc_file,
         #     header='WARC-Type',
@@ -38,10 +50,17 @@ for df in reader:
         #     header='WARC-Refers-To',
         #     value=warc_record.rec_headers.get_header('WARC-Record-ID')
         # )
+
+        # Get English WET record and print details
         wet_record = get_record_with_header(
             wet_file,
             header='WARC-Identified-Content-Language',
             value="eng"
         )
-        print(wet_record.rec_headers.get_header('WARC-Target-URI'))
-        print(wet_record.content_stream().read().decode('utf-8') + '\n')
+        # print(wet_record.rec_headers.get_header('WARC-Target-URI'))
+        # print(wet_record.content_stream().read().decode('utf-8') + '\n')
+
+        text = get_WET_text(wet_record)
+        if any(term in text for term in slurs):
+            print(wet_record.rec_headers.get_header('WARC-Target-URI'))
+            print(text)
