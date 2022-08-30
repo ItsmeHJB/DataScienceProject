0c24ee0cc8c3dc5ae0001a183ec5b45c24708f4b
ItsmeHJB <harrison.be@hotmail.co.uk>
2022-08-30 13:41:44 +0100

    Complete slur detection on small dataset

--- slur_detection.py
+++ slur_detection.py
@@ -17,7 +17,7 @@ def wrapper(gen):
         except StopIteration:
             break
         except Exception as e:
-            print(e)  # or whatever kind of logging you want
+            print(e)
 
 
 def get_record_with_header(warc_file, header, value):
@@ -29,9 +29,11 @@ def get_record_with_header(warc_file, header, value):
 
 def get_WET_text(record):
     if record:
+        # Reads text, decodes it using utf-8, makes it lowercase
         return record.content_stream().read().decode('utf-8').lower()
     else:
         return None
+
 
 prefix_url = 'https://data.commoncrawl.org/'
 warc_match = re.compile('warc')
@@ -48,35 +50,31 @@ else:
     skip = sorted(random.sample(range(1, f_len + 1), f_len - sample_size))
     slurs = pd.read_csv(slurs_file, skiprows=skip)['word'].tolist()
 
+cols = ['word', 'wet-url']
+matches = pd.DataFrame(columns=cols)
+filename = "matches.csv"
+
 # reader = pd.read_csv('Data/b75c614d-bb12-4083-9664-3ea424f6d4ce.csv', usecols=[0], chunksize=1000)
 reader = pd.read_csv('Data/4bc54b17-39ca-4264-b522-6f0c4f46f1f1.csv', usecols=[0], chunksize=1000)
 processed = 0
+found = 0
 start = time.perf_counter()
+
 for df in reader:
     for index, row in df.iterrows():
-        if processed <= 0:
+        # Use to skip to certain row index
+        if processed <= 542000:
             processed += 1
             continue
         if processed % 1000 == 0:
             print("processed: " + str(processed))
-            print(f"time: {time.strftime('%H:%M:%S', time.gmtime(time.perf_counter() - start))}\n")
+            print(f"time: {time.strftime('%H:%M:%S', time.gmtime(time.perf_counter() - start))}")
+            print("found: " + str(found) + "\n")
         # warc_url = prefix_url + row[0]
         # warc_file = get_file(warc_url)
 
         wet_url = prefix_url + re.sub(warc_match, ext_sub, re.sub(warc_match, wet_sub, row[0], count=1))
         wet_file = get_file(wet_url)
-
-        # Get WARC and associated WET record
-        # warc_record = get_record_with_header(
-        #     warc_file,
-        #     header='WARC-Type',
-        #     value='response'
-        # )
-        # wet_record = get_record_with_header(
-        #     wet_file,
-        #     header='WARC-Refers-To',
-        #     value=warc_record.rec_headers.get_header('WARC-Record-ID')
-        # )
 
         # Get English WET record and print details
         wet_record = get_record_with_header(
@@ -95,9 +93,18 @@ for df in reader:
             # Basic contains check - issues with offensive terms list and context
             for term in slurs:
                 if re.search(r'\b' + term + r'\b', text):
-                    print(wet_record.rec_headers.get_header('WARC-Target-URI'))
-                    print("Term: " + term)
-                    print(text[:25]+"\n")
+                    data = [term, wet_url]
+                    matches.loc[len(matches.index)] = data
+                    matches.to_csv(filename, mode='a', index=False, header=False)
+                    matches = matches[0:0]
+                    # print(wet_record.rec_headers.get_header('WARC-Target-URI'))
+                    # print("Term: " + term)
+                    # print(text[:25]+"\n")
+                    found += 1
+        # if matches.shape[0] >= 5:
+        #     matches.to_csv(filename, mode='a', index=False, header=False)
+        #     matches = matches[0:0]
         processed += 1
 
+matches.to_csv(filename, mode='a', index=False, header=False)
 print("Done :)")
